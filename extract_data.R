c477f69851799bc2c318901e37acd18654d910ac
hindrek <hindrek.teder@gmail.com>
2017-07-19 15:44:45 +0300

    replace for with lapply

--- extract_data.R
+++ extract_data.R
@@ -2,46 +2,35 @@ library(rvest)
 library(magrittr)
 
 
-html <- read_html("http://jalgpall.ee/voistlused/protocol/67480")
-
-
 players <- function(html, team = "Tartu Ülikool Fauna") {
   # home or away team
-  home <- html %>%
+  home <- read_html(html) %>%
     html_nodes("#page > div > div > div > div.head > div.teams > div:nth-child(1) > p") %>%
     html_text()
 
-  away <- html %>%
+  away <- read_html(html) %>%
     html_nodes("#page > div > div > div > div.head > div.teams > div:nth-child(3) > p") %>%
     html_text()
 
   side <- ifelse(team == home, "left", ifelse(team == away, "right", stop("Wrong team, check URL.", call. = F)))
 
   # line-up
-  lnp <- html %>%
+  lnp <- read_html(html) %>%
     html_nodes("#page > div > div > div > div:nth-child(4) > div.list.list-02.lineup.clearfix") %>%
     html_nodes(paste0("ul.", side, " > li > p > a")) %>%
     html_text()
 
   # substitutes
-  subs <- html %>%
+  subs <- read_html(html) %>%
     html_nodes("#page > div > div > div > div:nth-child(5) > div.list.list-02.clearfix") %>%
     html_nodes(paste0("ul.", side, " li"))
   subs <- subs[-1]
 
   # check which substitutes played
-  plyd <- vector()
-  for(sub in subs) {
+  lapply(subs, function(sub) {
     name <- html_nodes(sub, "a") %>%
-      html_text()
-
-    if(length(html_nodes(sub, "span.in"))) {
-      plyd <- c(plyd, name)
-    }
-  }
-
-  list(
-    game = paste(home, "-", away),
-    players = c(lnp, plyd)
-  )
+       html_text()
+    name[length(html_nodes(sub, "span.in"))]
+  }) %>%
+    unlist()
 }
