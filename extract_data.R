b9677c70e96c6ce87f380b28a29c066d512acf7c
hindrek <hindrek.teder@gmail.com>
2017-07-19 16:13:32 +0300

    html to url

--- extract_data.R
+++ extract_data.R
@@ -2,26 +2,26 @@ library(rvest)
 library(magrittr)
 
 
-players <- function(html, team = "Tartu Ülikool Fauna") {
+players <- function(url, team = "Tartu Ülikool Fauna") {
   # home or away team
-  home <- read_html(html) %>%
+  home <- read_html(url) %>%
     html_nodes("#page > div > div > div > div.head > div.teams > div:nth-child(1) > p") %>%
     html_text()
 
-  away <- read_html(html) %>%
+  away <- read_html(url) %>%
     html_nodes("#page > div > div > div > div.head > div.teams > div:nth-child(3) > p") %>%
     html_text()
 
   side <- ifelse(team == home, "left", ifelse(team == away, "right", stop("Wrong team, check URL.", call. = F)))
 
   # line-up
-  lnp <- read_html(html) %>%
+  lnp <- read_html(url) %>%
     html_nodes("#page > div > div > div > div:nth-child(4) > div.list.list-02.lineup.clearfix") %>%
     html_nodes(paste0("ul.", side, " > li > p > a")) %>%
     html_text()
 
   # substitutes
-  subs <- read_html(html) %>%
+  subs <- read_html(url) %>%
     html_nodes("#page > div > div > div > div:nth-child(5) > div.list.list-02.clearfix") %>%
     html_nodes(paste0("ul.", side, " li"))
   subs <- subs[-1]
