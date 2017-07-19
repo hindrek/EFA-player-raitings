affa6a62dbbeefbd044b9905b2ca345c0a24e297
hindrek <hindrek.teder@gmail.com>
2017-07-19 09:29:43 +0300

    merge home and away teams

--- extract_data.R
+++ extract_data.R
@@ -2,45 +2,30 @@ library(rvest)
 library(magrittr)
 
 
-html <- read_html("http://jalgpall.ee/voistlused/protocol/69204")
+team <- "EMÜ SK"
 
-lnps <- html %>%
-  html_nodes("#page > div > div > div > div:nth-child(4) > div.list.list-02.lineup.clearfix")
+url <- "http://jalgpall.ee/voistlused/protocol/69204" # readline("URL: ")
+html <- read_html(url)
 
-subs <- html %>%
-  html_nodes("#page > div > div > div > div:nth-child(5) > div.list.list-02.clearfix")
-
-# home team ---------------------------------------------------------------
-
-home <- lnps %>%
-  html_nodes("ul.left")
-
-home_team <- home %>%
-  html_nodes("li.title") %>%
+home <- html %>%
+  html_nodes("#page > div > div > div > div.head > div.teams > div:nth-child(1) > p") %>%
   html_text()
 
-home_lnp <- home %>%
-  html_nodes("li > p > a") %>%
+away <- html %>%
+  html_nodes("#page > div > div > div > div.head > div.teams > div:nth-child(3) > p") %>%
+  html_text()
+
+side <- ifelse(team == home, "left", ifelse(team == away, "right", stop("Wrong team, check URL.", call. = F)))
+
+lnp <- html %>%
+  html_nodes("#page > div > div > div > div:nth-child(4) > div.list.list-02.lineup.clearfix") %>%
+  html_nodes(paste0("ul.", side, " > li > p > a")) %>%
   html_text()
 
 # TODO: check which substitutes played
-home_subs <- subs %>%
-  html_nodes("ul.left > li > p > a") %>%
+subs <- html %>%
+  html_nodes("#page > div > div > div > div:nth-child(5) > div.list.list-02.clearfix") %>%
+  html_nodes(paste0("ul.", side, " > li > p > a")) %>%
   html_text()
 
-# away team ---------------------------------------------------------------
-
-away <- lnps %>%
-  html_nodes("ul.right")
-
-away_team <- away %>%
-  html_nodes("li.title") %>%
-  html_text()
-
-away_lnp <- away %>%
-  html_nodes("li > p > a") %>%
-  html_text()
-
-away_subs <- subs %>%
-  html_nodes("ul.right > li > p > a") %>%
-  html_text()
+plyrs <- c(lnp, subs)
